b4742c6b0fa1aeffcfb121944b273cede06b790b
Lam <endweed0@gmail.com>
2022-08-19 19:27:22 +0700

    remove public/css

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+//http loger
+app.use(morgan("combined"));
+
+//tempolate engine
+//su dung engine de doi ten duoi file
+app.engine(
+    "hbs",
+    hbs.engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources\\views"));
+
+//cho phep cac trang web tinh
+app.use(express.static(path.join(__dirname, "public")));
+
+//tao cac page moi
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/news", (req, res) => {
+    res.render("news");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
