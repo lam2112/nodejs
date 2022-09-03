7df828df603aafaffb642081076434f16362d792
unknown <endweed0@gmail.com>
2022-09-03 21:05:34 +0700

    course detail

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,9 +1,19 @@
+const Course = require("../models/Course");
+const { mogooseObject } = require("../../util/mongoose");
+
 class CourseController {
     // GET /course/:slug
-    show(req, res) {
-        res.send("courses");
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => {
+                res.render("course/show", {
+                    course: mogooseObject(course),
+                });
+            })
+            .catch((err) => {
+                next = next(err);
+            });
     }
-
 }
 
 module.exports = new CourseController();
